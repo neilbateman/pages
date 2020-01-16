8d2ddd74a551a9959566a645e4efc633d3b73099
Neil Bateman <Neil@Rivhas-MacBook-Pro.local>
2020-01-15 16:26:51 -0800

    Updates

---
+++ precache-manifest.07d3ce0f14d49d24096f6540ab43687c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a5073663485d8f47e764d63e9e2c05",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "5e4a6c57d861aa66e06d",
+    "url": "/pages/static/css/main.775fa37b.chunk.css"
+  },
+  {
+    "revision": "bfc07ae6b0380b4125b5",
+    "url": "/pages/static/js/2.4067dfd8.chunk.js"
+  },
+  {
+    "revision": "f05bfc683a175306602e6f609a238cdb",
+    "url": "/pages/static/js/2.4067dfd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e4a6c57d861aa66e06d",
+    "url": "/pages/static/js/main.71849425.chunk.js"
+  },
+  {
+    "revision": "ad1d5b063ba90eff0d5f",
+    "url": "/pages/static/js/runtime-main.a1037a1b.js"
+  }
+]);
